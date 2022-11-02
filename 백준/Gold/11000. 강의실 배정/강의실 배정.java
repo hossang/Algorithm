9e31bde2a33822e87d5670fd015d7f6d6ff1be9e
hossang <60059710+hossang@users.noreply.github.com>
2022-11-02 09:39:54 +0900

    [Gold V] Title: 강의실 배정, Time: 672 ms, Memory: 69464 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr,new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                if (a[0] == b[0]) {
+                    return a[1] - b[1];
+                }
+                return a[0] - b[0];
+            }
+        });
+        pq.add(arr[0][1]);
+        for (int i = 1; i < n; i++) {
+            if (arr[i][0] < pq.peek()) {
+                pq.add(arr[i][1]);
+            } else {
+                pq.poll();
+                pq.add(arr[i][1]);
+            }
+        }
+        System.out.println(pq.size());
+    }
+}
