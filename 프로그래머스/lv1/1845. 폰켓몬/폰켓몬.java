e7d0f44a8bb7231cbee2da580e052d88169afab2
hossang <60059710+hossang@users.noreply.github.com>
2022-11-22 18:02:55 +0900

    [level 1] Title: 폰켓몬, Time: 4.01 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int num : nums) {
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        if(nums.length/2>=map.size()) {
+            return map.size();
+        }
+        return nums.length/2;
+    }
+}
