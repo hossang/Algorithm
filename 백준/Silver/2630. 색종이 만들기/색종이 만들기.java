f2efcad0224b0a00aac26d63b6247f57542f4658
hossang <60059710+hossang@users.noreply.github.com>
2022-12-20 18:49:26 +0900

    [Silver II] Title: 색종이 만들기, Time: 172 ms, Memory: 15724 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[][] arr;
+    static int completewhite = 0;
+    static int completeblack = 0;
+    public static void recur(int hlt, int hrt, int vlt, int vrt) { //h - 가로(행) , v - 세로(열)
+        //언제까지?
+        if (hrt - hlt == 1 && vrt - vlt == 1) {
+            int w;
+            int b;
+            w = b = 0;
+            for (int i = hlt; i <= hrt; i++) {
+                for (int j = vlt; j <= vrt; j++) {
+                    if (arr[i][j] == 0) {
+                        w++;
+                    } else {
+                        b++;
+                    }
+                }
+            }
+            if (w == 0 || b == 0) { //한 영역이 모두 같으면
+                if (w == 0) {
+                    completeblack++;
+                } else {
+                    completewhite++;
+                }
+            } else { //다르면
+                completewhite += w;
+                completeblack += b;
+            }
+            return;
+        }
+
+        //★한 영역이 같은지 어캐 판단? -> 흰색과 검은색의 갯수?
+        int w;
+        int b;
+        w = b = 0;
+        for (int i = hlt; i <= hrt; i++) {
+            for (int j = vlt; j <= vrt; j++) {
+                if (arr[i][j] == 0) {
+                    w++;
+                } else {
+                    b++;
+                }
+            }
+        }
+        if (w == 0 || b == 0) { //한 영역이 모두 같으면
+            if (w == 0) {
+                completeblack++;
+            } else {
+                completewhite++;
+            }
+        } else { //다르면
+            recur(hlt, (hlt + hrt) / 2, vlt, (vlt + vrt) / 2); //1사분면
+            recur(hlt,(hlt + hrt) / 2,(vlt + vrt) / 2 + 1, vrt); //2사분면
+            recur((hlt + hrt) / 2 + 1, hrt, vlt, (vlt + vrt) / 2); //3사분면
+            recur((hlt + hrt) / 2 + 1, hrt, (vlt + vrt) / 2 + 1, vrt);//4사분면
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine()); // N은 2, 4, 8, 16, 32, 64, 128
+        arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) { //값 넣기
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        /*for (int i = 0; i < n; i++) {//print
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }*/
+        recur(0, n - 1, 0, n - 1);
+
+        System.out.println(completewhite);
+        System.out.println(completeblack);
+    }
+}
