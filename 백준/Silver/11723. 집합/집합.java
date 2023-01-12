4f1fedc06317063cb687567b7ebfa9d79712cc2d
hossang <60059710+hossang@users.noreply.github.com>
2023-01-12 19:02:13 +0900

    [Silver V] Title: 집합, Time: 1132 ms, Memory: 313004 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,53 +1,69 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
 
 public class Main {
-    static Set<Integer> si;
+
     public static void main(String[] args) throws IOException {
+        //초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-        si = new HashSet<>();
-        int x = Integer.parseInt(br.readLine());
-        for (int i = 0; i < x; i++) {
+        int M = Integer.parseInt(br.readLine());
+        int set = 0; // ...000000000000(2)
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
+            int param;
             switch (st.nextToken()) {
                 case "add":
-                    si.add(Integer.parseInt(st.nextToken()));
+                    param = Integer.parseInt(st.nextToken());
+                    set |= (1 << (param - 1));
                     break;
+
                 case "check":
-                    int z = si.contains(Integer.parseInt(st.nextToken())) ? 1 : 0;
-                    sb.append(z).append("\n");
-                    break;
-                case "remove":
-                    si.remove(Integer.parseInt(st.nextToken()));
-                    break;
-                case "toggle":
-                    int aa = Integer.parseInt(st.nextToken());
-                    if (si.contains(aa)) {
-                        si.remove(aa);
+                    param = Integer.parseInt(st.nextToken());
+                    if ((set & (1 << (param - 1))) != 0) {
+                        sb.append(1).append("\n");
                     } else {
-                        si.add(aa);
+                        sb.append(0).append("\n");
+
                     }
                     break;
+
+                case "remove":
+                    param = Integer.parseInt(st.nextToken());
+                    set &= ~(1 << (param - 1));
+                    break;
+
+                case "toggle":
+                    param = Integer.parseInt(st.nextToken());
+                    set ^= (1 << (param-1));
+                    break;
+
                 case "all":
-                    si.clear();
-                    for (int y = 1; y <= 20; y++) {
-                        si.add(y);
-                    }
+                    set |= (~0);
                     break;
+
                 case "empty":
-                    si.clear();
+                    set &= 0;
                     break;
+
                 default:
                     break;
             }
         }
-        System.out.print(sb);
-
-
+        System.out.println(sb);
 
     }
 }
