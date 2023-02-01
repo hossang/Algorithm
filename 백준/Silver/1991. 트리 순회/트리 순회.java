d3e0dae09c0d893882abfedbfa9e7c9678bea45a
hossang <60059710+hossang@users.noreply.github.com>
2023-02-01 10:46:02 +0900

    [Silver I] Title: 트리 순회, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,87 +1,94 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 class Node {
-    char data;
     Node left;
     Node right;
+    char data;
 
     public Node(char data) {
         this.data = data;
     }
-
 }
 
-public class Main {
-    private static class Tree {
-        Node root;
+class Tree {
+    Node root;
 
-        private void search(Node root, char parent, char left, char right) {
-            if (root == null) {
-                return;
-            }
-            if (root.data == parent) {
-                if (left != '.') {
-                    root.left = new Node(left);
-
-                }
-                if (right != '.') {
-                    root.right = new Node(right);
-                }
-            } else {
-                search(root.left, parent, left, right);
-                search(root.right, parent, left, right);
-            }
+    private void search(Node root, char parent, char left, char right) {
+        if (root == null) {
+            return;
         }
-
-        public void add(char parent, char left, char right) {
-            if (root == null) {
-                root = new Node(parent);
-                if (left != '.') {
-                    root.left = new Node(left);
-
-                }
-                if (right != '.') {
-                    root.right = new Node(right);
-                }
-            } else {
-                search(root, parent, left, right);
+        if (root.data == parent) {
+            if (left != '.') {
+                root.left = new Node(left);
             }
-        }
-
-        public void preorder(Node root) {
-            System.out.print(root.data);
-            if (root.left != null) {
-                preorder(root.left);
+            if (right != '.') {
+                root.right = new Node(right);
             }
-            if (root.right != null) {
-                preorder(root.right);
-            }
-        }
-
-        public void inorder(Node root) {
-            if (root.left != null) {
-                inorder(root.left);
-            }
-            System.out.print(root.data);
-            if (root.right != null) {
-                inorder(root.right);
-            }
-        }
-
-        public void postorder(Node root) {
-            if (root.left != null) {
-                postorder(root.left);
-            }
-            if (root.right != null) {
-                postorder(root.right);
-            }
-            System.out.print(root.data);
+        } else {
+            search(root.left, parent, left, right);
+            search(root.right, parent, left, right);
         }
     }
 
+    public void add(char parent, char left, char right) {
+        if (root == null) {
+            root = new Node(parent);
+            if (left != '.') {
+                root.left = new Node(left);
+            }
+            if (right != '.') {
+                root.right = new Node(right);
+            }
+        } else {
+            search(root, parent, left, right);
+        }
+    }
+
+    public void preorder(Node root) {
+        System.out.print(root.data);
+        if (root.left != null) {
+            preorder(root.left);
+        }
+        if (root.right != null) {
+            preorder(root.right);
+        }
+    }
+
+    public void inorder(Node root) {
+        if (root.left != null) {
+            inorder(root.left);
+        }
+        System.out.print(root.data);
+        if (root.right != null) {
+            inorder(root.right);
+        }
+    }
+
+    public void postorder(Node root) {
+        if (root.left != null) {
+            postorder(root.left);
+        }
+        if (root.right != null) {
+            postorder(root.right);
+        }
+        System.out.print(root.data);
+    }
+}
+
+public class Main {
     public static void main(String[] args) throws IOException {
         //초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -90,15 +97,16 @@ public class Main {
         Tree tree = new Tree();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            char parent = st.nextToken().charAt(0);
-            char left = st.nextToken().charAt(0);
-            char right = st.nextToken().charAt(0);
-            tree.add(parent, left, right);
+            char c1 = st.nextToken().charAt(0);
+            char c2 = st.nextToken().charAt(0);
+            char c3 = st.nextToken().charAt(0);
+            tree.add(c1, c2, c3);
         }
         tree.preorder(tree.root);
         System.out.println();
         tree.inorder(tree.root);
         System.out.println();
         tree.postorder(tree.root);
+
     }
 }
