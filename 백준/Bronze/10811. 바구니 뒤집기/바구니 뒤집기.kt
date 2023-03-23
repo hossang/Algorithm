09c9c417db2f307342604cd5d69f9de0a9c62691
hossang <60059710+hossang@users.noreply.github.com>
2023-03-24 00:12:22 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 92 ms, Memory: 12512 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.kt
@@ -0,0 +1,28 @@
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.util.*
+
+fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var sb = StringBuilder()
+    var st = StringTokenizer(readLine(), " ")
+    val N = st.nextToken().toInt()
+    val M = st.nextToken().toInt()
+    var arr = Array(N,{i -> i + 1})
+    for (h in 1..M) {
+        st = StringTokenizer(readLine(), " ")
+        var i = st.nextToken().toInt() - 1
+        var j = st.nextToken().toInt() - 1
+        var arr2 = Array(N, {0})
+        var cnt = j - i + 1
+        for (k in i..j) {
+            arr2[k] = arr[k]
+        }
+        for (k in 1..cnt) {
+            arr[i++] = arr2[j--]
+        }
+    }
+    arr.forEach { sb.append(it).append(" ") }
+    print(sb)
+
+}
