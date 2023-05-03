a207b7c58991f404d5168c892567bbaa6a4bea35
hossang <60059710+hossang@users.noreply.github.com>
2023-05-03 21:08:13 +0900

    [Gold III] Title: 줄 세우기, Time: 512 ms, Memory: 95444 KB -BaekjoonHub

---
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Flower{
+    int start;
+    int end;
+
+    public Flower(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
+
+public class Main {
+    private static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        int[] childs = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            childs[x] = childs[x - 1] + 1;
+        }
+        Arrays.sort(childs);
+        System.out.println(N - childs[N]);
+
+    }
+}
