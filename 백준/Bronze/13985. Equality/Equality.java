a118a67f76159c965838ed016579882e9352f916
hossang <60059710+hossang@users.noreply.github.com>
2023-06-07 01:42:54 +0900

    [Bronze IV] Title: Equality, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/13985. Equality/Equality.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        st.nextToken();
+        int b = Integer.parseInt(st.nextToken());
+        st.nextToken();
+        int c = Integer.parseInt(st.nextToken());
+        System.out.println(a+b==c?"YES":"NO");
+    }
+}
