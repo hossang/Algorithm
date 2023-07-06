9d7eef4ea0ffc65aeab64c0023f1d3e207a99af1
hossang <60059710+hossang@users.noreply.github.com>
2023-07-07 01:28:27 +0900

    [Bronze IV] Title: 한글 2, Time: 132 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    //백준
+    private static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st;
+
+        System.out.println(br.readLine().charAt(0)-44031);
+
+    }
+}
