e52ed9e0b10f0df6d1d7b94919f9c4aa9ca2898a
hossang <60059710+hossang@users.noreply.github.com>
2023-08-06 15:36:23 +0900

    [Silver V] Title: 영화감독 숌, Time: 296 ms, Memory: 86512 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int num = 666;
+        int count = 1;
+        while(count != n) {
+            num++;
+            if(String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
