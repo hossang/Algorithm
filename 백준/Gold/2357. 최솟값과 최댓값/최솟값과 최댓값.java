7bb12cc0043f0517c7aaa1b34b0fbf3f548a9d3c
hossang <60059710+hossang@users.noreply.github.com>
2023-08-08 00:48:04 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 872 ms, Memory: 67508 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.java
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.java
@@ -1,71 +1,68 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static int firstleaf;
-    private static int[] maxs, mins;
+    //백준
+    private static StringBuilder sb;
+    private static int firstLeaf;
+    private static int[] tree1, tree2;
     public static void main(String[] args) throws IOException {
-        //초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        sb = new StringBuilder();
         StringTokenizer st;
+
         st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        firstleaf = 1;
-        while (firstleaf < N) {
-            firstleaf <<= 1;
+
+        firstLeaf = 1;
+        while (firstLeaf < N) {
+            firstLeaf <<= 1;
         }
-        maxs = new int[firstleaf * 2];
-        Arrays.fill(maxs, Integer.MIN_VALUE);
-        mins = new int[firstleaf * 2];
-        Arrays.fill(mins, Integer.MAX_VALUE);
+
+        tree1 = new int[firstLeaf * 2]; //최소
+        Arrays.fill(tree1, (int) (1e9 + 1));
+        tree2 = new int[firstLeaf * 2]; //최대
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            maxs[firstleaf + i] = num;
-            mins[firstleaf + i] = num;
+            int x = Integer.parseInt(br.readLine());
+            tree1[firstLeaf + i] = x;
+            tree2[firstLeaf + i] = x;
         }
-        for (int i = firstleaf - 1; i >= 1; i--) { //maxtree
-            maxs[i] = Math.max(maxs[i * 2], maxs[i * 2 + 1]);
+        for (int i = firstLeaf - 1; i > 0; i--) {
+            tree1[i] = Math.min(tree1[i * 2], tree1[i * 2 + 1]);
+            tree2[i] = Math.max(tree2[i * 2], tree2[i * 2 + 1]);
         }
-        for (int i = firstleaf - 1; i >= 1; i--) { //mintree
-            mins[i] = Math.min(mins[i * 2], mins[i * 2 + 1]);
-        }
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            int sl = Integer.parseInt(st.nextToken());
-            int sr = Integer.parseInt(st.nextToken());
-            sb.append(searchMin(1, 1, firstleaf, sl, sr)).append(" ").append(searchMax(1, 1, firstleaf, sl, sr)).append("\n");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int min = minSearch(1, 1, firstLeaf, a, b);
+            int max = maxSearch(1, 1, firstLeaf, a, b);
+            sb.append(min).append(" ").append(max).append("\n");
         }
         System.out.println(sb);
     }
 
-    private static int searchMax(int node, int l, int r, int sl, int sr) {
-        //이 search 부분이 유일하게 어려워 (짜증짜증)
-        if (sl > r || sr < l) { //아예 겹치는 부분이 없는 부분
-            return Integer.MIN_VALUE;
+    private static int maxSearch(int now, int l, int r, int sl, int sr) {
+        if (sr < l || r < sl) {
+            return 0;
         }
-
-        //겹치는 부분이 있는 부분 중에
-        if (sl <= l && r <= sr) { //★ sl과 sr이 l과 r을 잡아 먹을 수 있으면
-            return maxs[node];
+        if (sl <= l && r <= sr) {
+            return tree2[now];
         }
-        //일부분만 겹치면
-        return Math.max(searchMax(node * 2, l, (l + r) / 2, sl, sr), searchMax(node * 2 + 1, (l + r) / 2 + 1, r, sl, sr));
+        return Math.max(maxSearch(now * 2, l, (l + r) / 2, sl, sr),
+                maxSearch(now * 2 + 1, (l + r) / 2 + 1, r, sl, sr));
     }
 
-    private static int searchMin(int node, int l, int r, int sl, int sr) {
-        //이 search 부분이 유일하게 어려워 (짜증짜증)
-        if (sl > r || sr < l) { //아예 겹치는 부분이 없는 부분
-            return Integer.MAX_VALUE;
+    private static int minSearch(int now, int l, int r, int sl, int sr) {
+        if (sr < l || r < sl) {
+            return (int) (1e9 + 1);
         }
-
-        //겹치는 부분이 있는 부분 중에
-        if (sl <= l && r <= sr) { //★ sl과 sr이 l과 r을 잡아 먹을 수 있으면
-            return mins[node];
+        if (sl <= l && r <= sr) {
+            return tree1[now];
         }
-        //일부분만 겹치면
-        return Math.min(searchMin(node * 2, l, (l + r) / 2, sl, sr), searchMin(node * 2 + 1, (l + r) / 2 + 1, r, sl, sr));
+        return Math.min(minSearch(now * 2, l, (l + r) / 2, sl, sr),
+                minSearch(now * 2 + 1, (l + r) / 2 + 1, r, sl, sr));
     }
 }
