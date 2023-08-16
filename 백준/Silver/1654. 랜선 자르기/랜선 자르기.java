bd98482447f59240cd3330f61559559260696d85
hossang <60059710+hossang@users.noreply.github.com>
2023-08-16 19:52:49 +0900

    [Silver II] Title: 랜선 자르기, Time: 176 ms, Memory: 17560 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,44 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    //백준
+    private static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int k = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-        int[] arr = new int[k];
+        sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[K];
+
         long right = 0;
-        
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < K; i++) {
             arr[i] = Integer.parseInt(br.readLine());
             if (right < arr[i]) {
                 right = arr[i];
             }
         }
 
-        long left = 0;
+        long left = 1;
         long mid = 0;
         right++;
 
-        while (left < right) {
+        while (left + 1 < right) {
             mid = (left + right) / 2;
             long count = 0;
 
             for (int v : arr) {
                 count += v / mid;
             }
-
-            if (count < n) {
-                right = mid;
-            } else {
-                left = mid + 1;
+            if (N <= count) {
+                left = mid;
+                continue;
             }
-        }
-        System.out.println(left - 1);
-    }
+            right = mid;
 
+        }
+        System.out.println(left);
+    }
 }
