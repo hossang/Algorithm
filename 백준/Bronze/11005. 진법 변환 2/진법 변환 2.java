3c7c0b71203484c962880268bf2f2a1e51b926a5
hossang <60059710+hossang@users.noreply.github.com>
2023-08-17 08:26:37 +0900

    [Bronze I] Title: 진법 변환 2, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    //백준
+    private static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(Integer.toString(N,B).toUpperCase()); //이런게 있네
+    }
+}
