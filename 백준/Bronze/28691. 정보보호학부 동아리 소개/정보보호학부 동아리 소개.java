76915b56bae9c2dfab1b7171187751273cc6af18
hossang <60059710+hossang@users.noreply.github.com>
2023-08-25 01:41:17 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 136 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    //백준
+    private static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st;
+
+        String s = br.readLine();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("M", "MatKor");
+        map.put("W", "WiCys");
+        map.put("C", "CyKor");
+        map.put("A", "AlKor");
+        map.put("$", "$clear");
+
+        System.out.println(map.get(s));
+        
+    }
+}
