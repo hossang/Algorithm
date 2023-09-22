9f4f8801fc2887e73943370847cb7341132b9f77
hossang <60059710+hossang@users.noreply.github.com>
2023-09-22 10:03:46 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    //백준
+    private static StringBuilder sb;
+    private static final Long INF = (long) (1e10 * 5 + 1);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st;
+
+        int B = Integer.parseInt(br.readLine());
+        System.out.println(B * 10 /11);
+    }
+}
