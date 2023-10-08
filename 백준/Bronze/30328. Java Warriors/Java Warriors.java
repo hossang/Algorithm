6f51042111a6e9d920f039721398bd551c243faf
hossang <60059710+hossang@users.noreply.github.com>
2023-10-09 01:54:51 +0900

    [Bronze V] Title: Java Warriors, Time: 120 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n*4000);
+    }
+}
