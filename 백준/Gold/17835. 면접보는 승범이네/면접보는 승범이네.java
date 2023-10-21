bafda09dc2c154c0287d9225f4a2a1b89b96ed4f
hossang <60059710+hossang@users.noreply.github.com>
2023-10-22 02:59:39 +0900

    [Gold II] Title: 면접보는 승범이네, Time: 1244 ms, Memory: 181116 KB -BaekjoonHub

--- 백준/Gold/17835. 면접보는 승범이네/면접보는 승범이네.java
+++ 백준/Gold/17835. 면접보는 승범이네/면접보는 승범이네.java
@@ -1,81 +1,83 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 class VE{
     int v;
     long e;
-    VE(int v, long e) {
+
+    public VE(int v, long e) {
         this.v = v;
         this.e = e;
     }
 }
 
 public class Main {
+    //백준
     private static StringBuilder sb;
-    private static long[] dist;
-    private static final long INF = 1_000_000_000_001L;
+    private static final Long INF = 1234567891231L;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
         StringTokenizer st;
 
-        st=new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
+        long[] dist = new long[N + 1];
+        Arrays.fill(dist, INF);
+        PriorityQueue<VE> pq = new PriorityQueue<>((o1, o2) -> {
+            if (o1.e - o2.e < 0) {
+                return -1;
+            }
+            if (o1.e - o2.e == 0) {
+                return 0;
+            }
+            return 1;
+        });
         List<List<VE>> graph = new ArrayList<>();
-        for(int i=0;i<=N;i++) {
+        for (int i = 0; i < N + 1; i++) {
             graph.add(new ArrayList<>());
         }
-        //그래프 역순
-        for(int i=0;i<M;i++) {
-            st=new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
             int U = Integer.parseInt(st.nextToken());
             int V = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
-            graph.get(V).add(new VE(U,C));
+            long C = Long.parseLong(st.nextToken());
+            graph.get(V).add(new VE(U, C));
         }
-        st=new StringTokenizer(br.readLine(), " ");
-        dist = new long[N+1];
-        Arrays.fill(dist,INF);
-        PriorityQueue<VE> pq = new PriorityQueue<>((o1,o2) -> Long.compare(o1.e,o2.e));
-        Set<Integer> set = new HashSet<>();
-        while(st.hasMoreTokens()) {
-            int e = Integer.parseInt(st.nextToken());
-            set.add(e);
-            dist[e] = 0;
-            pq.offer(new VE(e,0));
-
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < K; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            dist[x] = 0L;
+            pq.offer(new VE(x, 0L));
         }
 
-        while(!pq.isEmpty()) {
+        while (!pq.isEmpty()) {
             VE now = pq.poll();
 
-            if(now.e >dist[now.v]) {
+            if (dist[now.v] < now.e) {
                 continue;
             }
 
-            for(VE next : graph.get(now.v)) {
-                if(dist[next.v] > now.e +next.e) {
+            for (VE next : graph.get(now.v)) {
+                if (dist[next.v] > now.e + next.e) {
                     dist[next.v] = now.e + next.e;
-                    pq.offer(new VE(next.v,dist[next.v]));
+                    pq.offer(new VE(next.v, dist[next.v]));
                 }
             }
         }
-
-        int ans1 = 0;
-        long ans2 = 0;
-        for (int i = 1; i <= N; i++) {
-            if(set.contains(i)) {
-                continue;
-            }
-            if (ans2 < dist[i]) {
-                ans1 = i;
-                ans2 = dist[i];
+        long f = 0;
+        int idx = 0;
+        for (int i = 0; i < N + 1; i++) {
+            if (f < dist[i] && dist[i] != INF) {
+                f = dist[i];
+                idx = i;
             }
         }
-        System.out.println(ans1);
-        System.out.println(ans2);
+        sb.append(idx).append("\n").append(f);
+        System.out.println(sb);
     }
 }
